fixed minus sign in K_h of force_law fixed einsum in TwoPointInteraction

cardillo/force_laws/_base.py
@@ -63,12 +63,21 @@ class ScalarForceLawBase(ABC):
         )
 
     def _KN_h(self, t, q, u):
-        print(f"ScalarForceLaw: KN_h Not fully implemented!")
-        la_c_l = self._la_c_l(t, self.l(t, q), self.l_dot(t, q, u))
+        l = self.l(t, q)
+        l_dot = self.l_dot(t, q, u)
+
+        la_c = self._la_c(t, l, l_dot)
+        la_c_l = self._la_c_l(t, l, l_dot)
+        la_c_l_dot = self._la_c_l_dot(t, l, l_dot)
+
         W_l = self.subsystem.W_l(t, q).reshape(self.subsystem._nu)
         W2_l = self.subsystem.W2_l(t, q).reshape(self.subsystem._nu, self.subsystem._nu)
-        K = self.la_c(t, q, u) * W2_l + np.outer(W_l, la_c_l * W_l)
-        return K, np.zeros((self.subsystem._nu, self.subsystem._nu))
+
+        # TODO: test this and check if N is purely skew-symmetric!
+        # TODO: check all the signs below here!
+        K = -la_c * W2_l - np.outer(W_l, la_c_l * W_l)
+        N = -np.outer(W_l, W2_l @ u) * la_c_l_dot
+        return K, N
 
     def export(self, sol_i, **kwargs):
         return self.subsystem.export(sol_i, **kwargs)
@@ -95,6 +104,7 @@ class ScalarForceLawComplianceForm(ScalarForceLawBase):
             self.h = self._h
             self.h_q = self._h_q
             self.h_u = self._h_u
+            self.KN_h = self._KN_h
 
     @abstractmethod
     def _c(self, t, l, l_dot, la_c): ...

cardillo/interactions/two_point_interaction.py
@@ -208,10 +208,10 @@ class TwoPointInteraction:
         J2_P2 = self.J2_P2(t, q)
 
         W2 = np.zeros((self._nu, self._nu))
-        W2[:nu1, :nu1] = J_P1.T @ P @ J_P1 + np.einsum("i, ijk -> ij", -n, J2_P1)
+        W2[:nu1, :nu1] = J_P1.T @ P @ J_P1 + np.einsum("i, ijk -> jk", -n, J2_P1)
         W2[:nu1, nu1:] = -J_P1.T @ P @ J_P2
         W2[nu1:, :nu1] = J_P2.T @ P @ J_P1
-        W2[nu1:, nu1:] = J_P2.T @ P @ J_P2 + np.einsum("i, ijk -> ij", n, J2_P2)
+        W2[nu1:, nu1:] = J_P2.T @ P @ J_P2 + np.einsum("i, ijk -> jk", n, J2_P2)
         return W2
 
     def export(self, sol_i, **kwargs):