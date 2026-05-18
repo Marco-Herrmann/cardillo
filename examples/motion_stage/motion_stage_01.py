fixed wrong indices in Newton's jacobian; updated motion stages for new rod and blender export

examples/motion_stage/motion_stage_01.py
@@ -3,12 +3,12 @@ import numpy as np
 from pathlib import Path
 
 from cardillo import System
-from cardillo.rods.boostedCosseratRod import make_BoostedCosseratRod
-from cardillo.rods import (
+from cardillo.rods_new import (
     RectangularCrossSection,
     CrossSectionInertias,
     Simo1986,
     CircularCrossSection,
+    make_CosseratRod,
 )
 from cardillo.discrete import RigidBody, Frame
 from cardillo.constraints import RigidConnection, Prismatic
@@ -56,7 +56,7 @@ def motion_stage():
     # discretization and model
     system = System()
     nelement = 20
-    Cable = make_BoostedCosseratRod(
+    Cable = make_CosseratRod(
         # idx_constraints=[0, 1, 2],
     )
 
@@ -65,8 +65,8 @@ def motion_stage():
 
     # cross section
     cross_section = RectangularCrossSection(width=w, height=h)
-    A = cross_section.area
-    Ix, Iy, Iz = np.diag(cross_section.second_moment)
+    A = cross_section.area(0.0)
+    Ix, Iy, Iz = np.diag(cross_section.second_moment(0.0))
 
     # TODO: get this into Simo
     Ei = np.array([E * A, G * A, G * A])
@@ -227,7 +227,7 @@ def motion_stage():
 
     # vtk-export
     dir_name = Path(__file__).parent
-    system.export(dir_name, "vtk_01", sol_dyn, fps=25)
+    system.export_blender(dir_name, "blender_01", sol_dyn, create_blend=True)
 
     # make nice visuals with multiple individual cables
     ny = 10
@@ -276,7 +276,7 @@ def motion_stage():
         t=sol_dyn.t,
         q=q_mult,
     )
-    system_mult.export(dir_name, "vtk_01_multiple", sol_mult, fps=25)
+    system_mult.export_blender(dir_name, "blender_01_multiple", sol_mult, create_blend=True)
 
 
 if __name__ == "__main__":

examples/motion_stage/motion_stage_02.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 from cardillo import System
 from cardillo.actuators.constraint import ActuatedConstraint
-from cardillo.rods.boostedCosseratRod import make_BoostedCosseratRod
-from cardillo.rods import (
+from cardillo.rods_new import (
     RectangularCrossSection,
     CrossSectionInertias,
     Simo1986,
     CircularCrossSection,
+    make_CosseratRod,
 )
 from cardillo.discrete import RigidBody, Frame
 from cardillo.constraints import RigidConnection, Prismatic
@@ -102,7 +102,7 @@ def motion_stage(l_x0, l_y0):
     nelement1 = 20
     nelement2a = 20
     nelement2b = 20
-    Cable = make_BoostedCosseratRod()
+    Cable = make_CosseratRod()
 
     t1 = 1.0
     dt = 0.001
@@ -161,8 +161,8 @@ def motion_stage(l_x0, l_y0):
 
     # cross section
     cross_section = RectangularCrossSection(width=8 * r_cable, height=2 * r_cable)
-    A = cross_section.area
-    Ix, Iy, Iz = np.diag(cross_section.second_moment)
+    A = cross_section.area(0.0)
+    Ix, Iy, Iz = np.diag(cross_section.second_moment(0.0))
 
     # TODO: get this into Simo
     Ei = np.array([E * A, G * A, G * A])
@@ -377,7 +377,7 @@ def motion_stage(l_x0, l_y0):
 
     # vtk-export
     dir_name = Path(__file__).parent
-    system.export(dir_name, "vtk_02", sol_dyn, fps=25)
+    system.export_blender(dir_name, "blender_02", sol_dyn, create_blend=True)
 
     # make nice visuals with multiple individual cables
     if False: