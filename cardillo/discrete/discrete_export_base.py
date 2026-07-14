dee0b38b09be859a7aab9eb3b71b21090f88f7bc
Marco Herrmann <m.herrmann@tue.nl>
2026-07-14 11:22:13 +0200

    export of arrow/block without in-between node

--- cardillo/discrete/discrete_export_base.py
+++ cardillo/discrete/discrete_export_base.py
@@ -240,13 +240,10 @@ def make_glTF_arrow(path, name, t, r_OP, vec, block=False):
     filename = os.path.join(path, f"{name}.glb")
 
     buf = BufferBuilder()
-    nodes = []
-    # create an empty
-    if True:
-        nodes.append(Node(name=f"{name}_obj__invisible"))
-        mesh_gltf = []
-
     t_acc = buf.add(t.astype(np.float32), 5126, "SCALAR")
+
+    # create node
+    node = Node(name=f"{name}_block" if block else f"{name}_vec")
 
     # translation
     trans = cardillo_to_gltf_trans(r_OP + (vec/2 if block else 0.0))
@@ -261,55 +258,50 @@ def make_glTF_arrow(path, name, t, r_OP, vec, block=False):
         ),
     ]
 
-    if True:
-        the_name = f"{name}_block" if block else f"{name}_vec"
-        nodes.append(Node(name=the_name))
-        nodes[0].children.append(len(nodes) - 1)
+    # find the quaternion that rotates the +z of the node to the direction of vec
+    # TODO: vectorize
+    P = np.array([smallest_rotation_quaternion(veci, i=2)[0] for veci in vec])
+    P_rot = cardillo_to_gltf_rot(P)
+    P_acc = buf.add(P_rot, 5126, "VEC4")
 
-        # find the quaternion that rotates the +z of the node to the direction of vec
-        # TODO: vectorize
-        P = np.array([smallest_rotation_quaternion(veci, i=2)[0] for veci in vec])
-        P_rot = cardillo_to_gltf_rot(P)
-        P_acc = buf.add(P_rot, 5126, "VEC4")
+    samplers.append(
+        AnimationSampler(input=t_acc, output=P_acc, interpolation="LINEAR")
+    )
+    channels.append(
+        AnimationChannel(
+            sampler=len(samplers) - 1,
+            target=AnimationChannelTarget(node=0, path="rotation"),
+        )
+    )
 
-        samplers.append(
-            AnimationSampler(input=t_acc, output=P_acc, interpolation="LINEAR")
+    # set the scale of the node to the magnitude of vec
+    vec_mag = np.linalg.norm(vec, axis=1).astype(np.float32)
+    if block:
+        # TODO: make this via cardillo_to_glTF_scale or so, such that we can here have [1/100, 1, 1/100]
+        vec_mag /= 2
+        scale = np.vstack([vec_mag / 100, vec_mag, vec_mag / 100]).T
+    else:
+        scale = np.stack([vec_mag] * 3, axis=1)
+    vec_acc = buf.add(scale, 5126, "VEC3")
+    samplers.append(
+        AnimationSampler(input=t_acc, output=vec_acc, interpolation="LINEAR")
+    )
+    channels.append(
+        AnimationChannel(
+            sampler=len(samplers) - 1,
+            target=AnimationChannelTarget(node=0, path="scale"),
         )
-        channels.append(
-            AnimationChannel(
-                sampler=len(samplers) - 1,
-                target=AnimationChannelTarget(node=len(nodes) - 1, path="rotation"),
-            )
-        )
-
-        # set the scale of the node to the magnitude of vec
-        vec_mag = np.linalg.norm(vec, axis=1).astype(np.float32)
-        if block:
-            vec_mag /= 2
-            scale = np.vstack([vec_mag / 100, vec_mag, vec_mag / 100]).T
-        else:
-            scale = np.stack([vec_mag] * 3, axis=1)
-        vec_acc = buf.add(scale, 5126, "VEC3")
-        samplers.append(
-            AnimationSampler(input=t_acc, output=vec_acc, interpolation="LINEAR")
-        )
-        channels.append(
-            AnimationChannel(
-                sampler=len(samplers) - 1,
-                target=AnimationChannelTarget(node=len(nodes) - 1, path="scale"),
-            )
-        )
+    )
 
     anim = Animation(samplers=samplers, channels=channels, name=f"{name}_anim")
     gltf = GLTF2(
         buffers=[Buffer(byteLength=len(buf.data))],
         bufferViews=buf.views,
         accessors=buf.accessors,
-        nodes=nodes,
+        nodes=[node],
         animations=[anim],
         scenes=[Scene(nodes=[0])],
         scene=0,
-        meshes=mesh_gltf,
     )
 
     gltf.set_binary_blob(buf.data)
